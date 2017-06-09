b3678df7822ece8564fc3c3da8f4d86cc480d599
VVM <vvm@vvm.laptop>
2017-06-09 22:57:00 +0500

    Added secret config validation

--- protected/app/config/prod.php
+++ protected/app/config/prod.php
@@ -1,2 +1,8 @@
 <?php
-require_once __DIR__ . '/secret.php';
+
+// DATABASE, API TOKENS, ETC
+
+$secret_path = __DIR__ . '/secret.php';
+if(file_exists($secret_path)){
+	require_once $secret_path;	
+}
